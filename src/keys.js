a3b5ff4155a5b35c533a770a4443e33e95b853d3
AresDeLaCruz <aresdelacruzg@gmail.com>
2023-09-21 00:29:27 -0600

    make

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports={
     database:{
-        host:'dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user:'xkhfx3nqjqhasjeq',
-        password:'as8g3e0b427nfc7q',
-        database:'lbgcs6z73v9468ig'
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'webapp'
     }
 }
